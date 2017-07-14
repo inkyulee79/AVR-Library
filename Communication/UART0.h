d2ced101724ce12400ed1560d560e1dc22bd466f
Inkyu Lee <123inn9@naver.com>
2017-07-14 12:15:33 +0900

    주석 추가 및 함수 이름 변경

--- Communication/UART0.h
+++ Communication/UART0.h
@@ -1,9 +1,14 @@
-﻿
+﻿/*
+UART 통신은 가장 많이 사용되는 통신 방식입니다.
+이 통신은 AVR과 PC 간 통신을 활성시킵니다.
+UART communication is a communication way in AVR system.
+This enables a communication between AVR and PC.
+*/
 
 #ifndef UART0_H_
 #define UART0_H_
 
-void UART0_init() 
+void initUART0_default() 
 {
 	UBRR0H = 0x00;
 	UBRR0L = 207;
@@ -17,35 +22,35 @@ void UART0_init()
 	
 }
 
-void UART0_transmit(char data) 
+void transmitUART0(char data) 
 {
 	while(!(UCSR0A & (1<<UDRE0)));
 	UDR0 = data;
 }
 
-void UART0_transmit_string(char* str)
+void transmitUART0_string(char* str)
 {
 	int i;
 	for(i=0;str[i]!='\0';i++)
 	{
-		UART0_transmit(str[i]);
+		transmitUART0(str[i]);
 	}
 }
 
-unsigned char UART0_receive()
+unsigned char receiveUART0()
 {
 	while(! (UCSR0A & (1<<RXC0)) );
 	return UDR0;
 }
 
-int is_UART0_received()
+int isUART0Received()
 {
 	return ((UCSR0A >> TXEN0) & 1);
 }
 
 FILE OUTPUT \
-= FDEV_SETUP_STREAM(UART0_transmit, NULL, _FDEV_SETUP_WRITE);
+= FDEV_SETUP_STREAM(transmitUART0, NULL, _FDEV_SETUP_WRITE);
 FILE INPUT \
-= FDEV_SETUP_STREAM(NULL, UART0_receive, _FDEV_SETUP_READ);
+= FDEV_SETUP_STREAM(NULL, receiveUART0, _FDEV_SETUP_READ);
 
 #endif /* UART0_H_ */
